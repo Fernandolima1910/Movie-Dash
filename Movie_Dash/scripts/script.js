c15cb864d205c90259250e33d7ac2a29bc694c70
Fernando Lima <lima2007.f@gmail.com>
2026-04-11 11:50:55 -0300

    Adicionei cores ao front END

---
+++ Movie_Dash/scripts/script.js
@@ -0,0 +1,23 @@
+
+
+function acess_home(){
+    alert("foi")    
+    const home = document.getElementById("home")
+    home.style.backgroundColor = 'black'
+}
+
+function acess_search(){
+    alert("foi")
+}
+function acess_profile(){
+    alert("foi")
+}
+function acess_watchlist(){
+    alert("foi")
+}
+function acess_match_room(){
+    alert("foi")
+}
+function acess_speanding(){
+    alert("foi")
+}
